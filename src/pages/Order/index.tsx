4013f677adf620e0e44946941c881355b53a855a
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2024-12-22 11:20:26 -0300

    Adicionado modal categoria

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -4,11 +4,13 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  TextInput
+  TextInput,
+  Modal
 } from "react-native";
 import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import { api } from "../../services/api";
+import { ModalPicker } from "../../components/ModalPicker";
 
 type RouteDetailParams = {
   Order: {
@@ -17,7 +19,7 @@ type RouteDetailParams = {
   }
 }
 
-type CategoryProps = {
+export type CategoryProps = {
   id: string;
   name: string;
 }
@@ -30,6 +32,7 @@ export default function Order() {
 
   const [category, setCategory] = useState<CategoryProps[] | []>([])
   const [categorySelected, setCategorySelected] = useState<CategoryProps>()
+  const [modalCategoryVisible, setModalCategoryVisible] = useState(false)
 
   const [amount, setAmount] = useState('1')
 
@@ -56,6 +59,10 @@ export default function Order() {
     }
   }
 
+  function handleChangeCategory(item: CategoryProps) {
+    setCategorySelected(item)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -66,7 +73,7 @@ export default function Order() {
       </View>
 
       {category.length !== 0 && (
-        <TouchableOpacity style={styles.input}>
+        <TouchableOpacity style={styles.input} onPress={ () => setModalCategoryVisible(true) }>
           <Text style={{ color: '#FFF' }}>
             {categorySelected?.name}
           </Text>
@@ -97,6 +104,18 @@ export default function Order() {
           <Text style={styles.buttonText}>Avançar</Text>
         </TouchableOpacity>
       </View>
+
+      <Modal
+        transparent={true}
+        visible={modalCategoryVisible}
+        animationType="fade"
+      >
+        <ModalPicker 
+          handleCloseModal={ () => setModalCategoryVisible(false) }
+          options={category}
+          selectedItem={ handleChangeCategory }
+        />
+      </Modal>
 
     </View>
   );
